19a0eae5c1cc15fb322c60c77ba92adca07c7086
vakrilov <alexander.vakrilov@telerik.com>
2017-07-08 12:51:10 +0300

    styleing

--- app/components/contest-list.component.ts
+++ app/components/contest-list.component.ts
@@ -6,11 +6,14 @@ Vue.component('contest-component', {
     props: [ 'contest' ],
     template: `
     <grid-layout>
-        <card-view class="contest-card">
-            <grid-layout rows="auto auto auto" @tap="$emit('selected')">
-                <image :src="contest.imageUrl" height="160" stretch="aspectFill"></image>
-                <label :text="contest.name" row="1" class="h2" margin="24 16 0 16"></label>
-                <label :text="contest.description" row="2" textWrap="true" margin="12 16 24 16"></label>
+        <card-view class="card">
+            <grid-layout rows="auto auto auto" columns="* auto" @tap="$emit('selected')">
+                <image :src="contest.imageUrl" height="160" stretch="aspectFill" colSpan="2"></image>
+                <label :text="contest.name" row="1" class="h2" margin="24 4 0 16" textWrap="true"></label>
+                <label row="1" col="1" class="font-weight-bold pull-right" margin="32 16 0 0"
+                    :class="contest.hasEnded ? 'ended' : 'current'" 
+                    :text="contest.hasEnded ? 'ENDED' : 'CURRENT'"></label>
+                <label :text="contest.description" row="2" textWrap="true" margin="12 16 24 16" colSpan="2"></label>
             </grid-layout>
         </card-view>
     </grid-layout>
