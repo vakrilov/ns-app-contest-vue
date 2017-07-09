085210af2b99d91fef3091c4d52c73cc8dce590e
vakrilov <alexander.vakrilov@telerik.com>
2017-07-10 01:36:46 +0300

    App list

--- app/components/app-list.component.ts
+++ app/components/app-list.component.ts
@@ -1,7 +1,53 @@
+const Vue = require('nativescript-vue/dist/index')
+
 import { screen } from "tns-core-modules/platform";
 import { AppEntry, getApps, toggleVote, startAppListeners, stopAppListeners } from "../services/backend.service";
 import { currentUser } from "../services/user.service";
 
+
+Vue.component('app-component', {
+    props: [ 'app' ],
+    computed: {
+        // a computed getter
+        voteImageSrc: function () {
+            if (currentUser.id) {
+                if (this.app.userVoted) {
+                    return "res://heart_filled";
+                } else {
+                    return "res://heart_empty";
+                }
+            } else {
+                return "res://heart_inactive"
+            }
+        }
+    },
+    template: `
+    <grid-layout>
+        <card-view class="card">
+            <grid-layout rows="auto auto auto auto auto auto" columns="* auto" @tap="$emit('selected')">
+                <image :src="'https://d2odgkulk9w7if.cloudfront.net/images/default-source/default-album/screen696x6961046632a7b776b26a649ff04000922f2.jpeg'" height="160" stretch="aspectFill" colSpan="2"></image>
+                <label :text="app.name" row="1" class="h2" margin="24 16 0 16" textWrap="true"></label>
+                <label :text="'by ' + app.author" row="2" textWrap="true" margin="12 16 24 16" colSpan="2"></label>
+
+                <stack-layout row="3" horizontalAlignment="right" orientation="horizontal">
+                    <image src="res://fullscreen" class="icon"></image>
+                
+                    <grid-layout columns="auto auto" @tap="$emit('vote')">
+                        <image :src="voteImageSrc" class="icon"></image>
+                        <label :text="app.votes" col="1" :class="app.userVoted ? 'voted' : 'not-voted'"></label>
+                    </grid-layout>
+                </stack-layout>
+            </grid-layout>
+        </card-view>
+    </grid-layout>
+    `
+})
+
+// <list-view :items="items" separatorColor="transparent" backgroundColor="transparent">
+//     <template scope="item">
+//         <app-component :app="item" @vote="vote(item)"></app-component>
+//     </template>
+// </list-view>
 export const AppListComponent = {
     props: [ "contestId" ],
     data: () => {
@@ -15,15 +61,9 @@ export const AppListComponent = {
     template: `
     <grid-layout>
         <scroll-view>
-            <wrap-layout orientation="horizontal" :itemWidth="itemWidth">
-                <stack-layout rows="auto auto auto" v-for="app in items" key="app.id" backgroundColor="lightgreen" margin="8">
-                    <label :text="app.name" row="0" class="h2" textWrap="true"></label>
-                    <label :text="'by ' + app.author" ></label>
-                    <label :text="'votes: ' + app.votes" row="1" ></label>
-                    <label :text="'userVoted: ' + app.userVoted" ></label>
-                    <button text="vote" @tap="vote(app)" row="2"></button>
-                </stack-layout>
-            </wrap-layout>
+            <stack-layout>
+                <app-component v-for="item in items" key="item.id" :app="item" @vote="vote(item)"></app-component>
+            </stack-layout>
         </scroll-view>
         <activity-indicator busy="true" v-if="loading"></activity-indicator>
     </grid-layout>
